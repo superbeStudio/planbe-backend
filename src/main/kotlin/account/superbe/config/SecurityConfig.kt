d910916e2bf8f973b0614f3c5352ed26ce185796
jinhee <bis97077@rubric.im>
2024-04-07 22:17:29 +0900

    SecurityConfig 설정

---
+++ src/main/kotlin/account/superbe/config/SecurityConfig.kt
@@ -0,0 +1,54 @@
+package kr.co.fnf.serp.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import java.time.Duration
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests {
+                authorizeRequests ->
+                    authorizeRequests
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().permitAll()
+            }
+            .csrf { csrf: CsrfConfigurer<HttpSecurity> -> csrf.disable() }
+        http
+                .headers{headerConfig ->
+                    headerConfig.frameOptions{frameOptions -> frameOptions.disable()}
+                }
+                .cors { corsConfigurationSource: CorsConfigurer<HttpSecurity?> -> corsConfigurationSource.configurationSource(corsConfigurationSource()) }
+
+        return http.build()
+    }
+
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val urlBasedCorsConfigurationSource = UrlBasedCorsConfigurationSource()
+
+        val corsConfiguration = CorsConfiguration()
+        corsConfiguration.addAllowedMethod(CorsConfiguration.ALL)
+        corsConfiguration.addAllowedHeader(CorsConfiguration.ALL)
+
+        corsConfiguration.setAllowedOrigins(listOf("/**"))
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration)
+
+        corsConfiguration.allowCredentials = true
+        corsConfiguration.setMaxAge(Duration.ofHours(2))
+
+        return urlBasedCorsConfigurationSource
+    }
+
+}
---
+++ src/main/kotlin/account/superbe/config/WebConfig.kt
@@ -0,0 +1,24 @@
+package account.superbe.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name()
+                )
+                .allowedHeaders("*")
+                .allowCredentials(true)
+    }
+}
