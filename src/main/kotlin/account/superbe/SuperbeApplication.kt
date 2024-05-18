55786d4e52cdb6fe432a7d2b0e702fcce1ec3959
jinhee <bis97077@rubric.im>
2024-05-18 17:32:17 +0900

    swagger 설정 추가

--- src/main/kotlin/account/superbe/SuperbeApplication.kt
+++ src/main/kotlin/account/superbe/SuperbeApplication.kt
@@ -1,10 +1,27 @@
 package account.superbe
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.security.SecurityScheme
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.context.annotation.Configuration
 
+@OpenAPIDefinition(
+        info = Info(title = "PLANBE API 명세서",
+                version = "v0.0.1",
+                contact = Contact(
+                        name = "superbe",
+                        email = "superbestudio@gmail.com"
+                )
+        )
+)
+@SecurityScheme(type = SecuritySchemeType.APIKEY, `in` = SecuritySchemeIn.HEADER, name = "Api-Token", description = "Auth Token")
 @SpringBootApplication
 class SuperbeApplication
 
--- src/main/kotlin/account/superbe/config/SecurityConfig.kt
+++ src/main/kotlin/account/superbe/config/SecurityConfig.kt
@@ -51,7 +51,7 @@ class SecurityConfig(private val jwtFilter: JwtFilter) {
         corsConfiguration.addAllowedMethod(CorsConfiguration.ALL)
         corsConfiguration.addAllowedHeader(CorsConfiguration.ALL)
 
-        corsConfiguration.allowedOrigins = listOf("/**", "http://localhost:3000")
+        corsConfiguration.allowedOrigins = listOf("/**", "http://localhost:3000", "/swagger-ui/**")
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration)
 
         corsConfiguration.allowCredentials = true
