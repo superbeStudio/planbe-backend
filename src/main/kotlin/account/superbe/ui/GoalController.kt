4e8eae719b1ff6d70282438a92586d6a8a9d0cb1
jinhee <bis97077@rubric.im>
2024-05-18 18:37:53 +0900

    goal path 수정

--- src/main/kotlin/account/superbe/ui/GoalController.kt
+++ src/main/kotlin/account/superbe/ui/GoalController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/goal")
 @RequiredArgsConstructor
 @Validated
 class GoalController (private val goalService: GoalApplicationService){
