cb5e5898d4bbe4c1fb828dcf23e92f527cdb7b7a
jinhee <bis97077@rubric.im>
2024-06-06 18:40:56 +0900

    slack error WEBHOOK URL 수정

--- src/main/kotlin/account/superbe/common/exception/ExceptionHandler.kt
+++ src/main/kotlin/account/superbe/common/exception/ExceptionHandler.kt
@@ -3,12 +3,14 @@ package account.superbe.common.exception
 import account.superbe.common.io.ResponseDto
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.client.RestTemplate
 
@@ -24,30 +26,31 @@ class ExceptionHandler {
                 .body(ResponseDto(message = e.message))
     }
 
-    @ExceptionHandler(Exception::class)
+    @ExceptionHandler(Throwable::class, DataIntegrityViolationException::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     fun handle500Exception(e: Exception): ResponseEntity<ResponseDto<Nothing>> {
-        reportErrorToSlack(e.message!!)
+        reportErrorToSlack(e.message)
         return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ResponseDto(message = e.message))
     }
 
-    private val WEBHOOK_URL = "https://hooks.slack.com/services/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"
+    private val WEBHOOK_URL = "https://hooks.slack.com/services/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"
     private val APP_NAME = "planbe"
     private val ERROR_CHANNEL = "backend-error-log"
     private val ICON_DANGER = ":rotating_light:"
-    private fun reportErrorToSlack(message: String) {
+    private fun reportErrorToSlack(message: String?) {
         if (listOf(*env.activeProfiles).contains("dev")) {
             try {
                 val restTemplate = RestTemplate()
                 val parameters: MutableMap<String, Any> = HashMap()
                 parameters["icon_emoji"] = ICON_DANGER
                 parameters["username"] = APP_NAME
-                parameters["text"] = message
+                parameters["text"] = message ?: "NO MESSAGE"
                 parameters["channel"] = "#$ERROR_CHANNEL"
 
                 val entity = HttpEntity<Map<String, Any>>(parameters)
-                restTemplate.exchange<String>(WEBHOOK_URL, HttpMethod.POST, entity, String::class.java)
+                restTemplate.exchange(WEBHOOK_URL, HttpMethod.POST, entity, String::class.java)
             } catch (e: java.lang.Exception) {
                 e.printStackTrace()
             }
