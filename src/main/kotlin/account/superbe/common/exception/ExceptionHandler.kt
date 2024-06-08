f761da940e04b2b7b33bd0bf6e1924879636e627
jinhee <bis97077@rubric.im>
2024-06-08 13:26:49 +0900

    ExceptionHandler 예외 에러 추가

--- src/main/kotlin/account/superbe/common/exception/ExceptionHandler.kt
+++ src/main/kotlin/account/superbe/common/exception/ExceptionHandler.kt
@@ -2,6 +2,8 @@ package account.superbe.common.exception
 
 import account.superbe.common.io.ResponseDto
 import io.sentry.Sentry
+import jakarta.validation.ConstraintViolationException
+import org.springframework.beans.TypeMismatchException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.json.JsonParseException
 import org.springframework.core.env.Environment
@@ -11,18 +13,39 @@ import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.HttpMediaTypeNotSupportedException
+import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.MissingServletRequestParameterException
+import org.springframework.web.bind.UnsatisfiedServletRequestParameterException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
+import org.springframework.web.multipart.MultipartException
+import org.springframework.web.multipart.support.MissingServletRequestPartException
 
 @RestControllerAdvice
 class ExceptionHandler {
     @Autowired
     lateinit var env: Environment
 
-    @ExceptionHandler(IllegalArgumentException::class, MethodArgumentNotValidException::class, JsonParseException::class, HttpMessageNotReadableException::class)
+    @ExceptionHandler(IllegalArgumentException::class,
+            MethodArgumentNotValidException::class,
+            JsonParseException::class,
+            HttpMessageNotReadableException::class,
+            HttpMediaTypeNotSupportedException::class,
+            HttpMessageNotReadableException::class,
+            MissingServletRequestPartException::class,
+            MissingServletRequestParameterException::class,
+            ConstraintViolationException::class,
+            TypeMismatchException::class,
+            MethodArgumentTypeMismatchException::class,
+            MultipartException::class,
+            UnsatisfiedServletRequestParameterException::class,
+            HttpRequestMethodNotSupportedException::class
+    )
     fun handleException(e: Exception): ResponseEntity<ResponseDto<Nothing>> {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
